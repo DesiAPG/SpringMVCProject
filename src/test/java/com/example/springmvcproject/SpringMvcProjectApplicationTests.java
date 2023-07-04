4577f9e718a02b2cc955992f9056e4ddfdc124a2
Desireeee <74121435+DesiAPG@users.noreply.github.com>
2023-07-04 17:10:30 -0500

    First Commit

---
+++ src/test/java/com/example/springmvcproject/SpringMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
